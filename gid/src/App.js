1e57ec3fada2970da4ae5d2635e92532a1879e84
jacen green <jacen.l.green@hotmail.com>
2024-09-22 11:27:25 +0100

    initial install of react

---
+++ gid/src/App.js
@@ -0,0 +1,17 @@
+import logo from './gid-logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Get It Done
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
