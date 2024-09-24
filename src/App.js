5b6ae734d3bf869d563651fcea123b66b619717f
jacen green <jacen.l.green@hotmail.com>
2024-09-24 21:29:32 +0100

    add routing for nav links and add custom transition-hover effects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render={()=> <h1>GET IT DONE</h1>} />
+          <Route exact path='/sign-in' render={()=> <h1>Sign In</h1>} />
+          <Route exact path='/new-user' render={()=> <h1>New User</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,31 +2,44 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/gid-logo.png';
 import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand='md' fixed='top'>
       <Container fluid='xxl'>
-        <Navbar.Brand className='m-0'>
-          <img src={logo} alt='logo' height='70' />
-        </Navbar.Brand>
-          <div className={styles.Logo}>
-            <h1 className='d-md-1'>GET IT DONE</h1>
-          </div>
+        <NavLink to='/'>
+          <Navbar.Brand className='m-0'>
+            <img src={logo} alt='logo' height='70' />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls='basic-navbar-nav' className={styles.NavButton}>
           <i class="fa-solid fa-caret-down"></i>
         </Navbar.Toggle>
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='m-auto text-start'>
-            <Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to='/'
+            >
               <i className='fas fa-fw fa-circle-h'></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to='/sign-in'
+            >
               <i className='fas fa-fw fa-circle-user'></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink 
+              className={styles.NavLink} 
+              activeClassName={styles.Active} 
+              to='/new-user'
+            >
               <i className='fas fa-fw fa-circle-plus'></i>New User
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
