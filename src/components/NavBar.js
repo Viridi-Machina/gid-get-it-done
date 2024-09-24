b5ba39121ce7faa1524605766332f4d66fec2143
jacen green <jacen.l.green@hotmail.com>
2024-09-24 19:31:49 +0100

    modularise css structure, add custom styling to nav header and button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,31 @@
-import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from "../assets/gid-logo.png";
+import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import logo from '../assets/gid-logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand='md' fixed='top'>
       <Container fluid='xxl'>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="100" />
+        <Navbar.Brand className='m-0'>
+          <img src={logo} alt='logo' height='70' />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto text-start">
+          <div className={styles.Logo}>
+            <h1 className='d-md-1'>GET IT DONE</h1>
+          </div>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' className={styles.NavButton}>
+          <i class="fa-solid fa-caret-down"></i>
+        </Navbar.Toggle>
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='m-auto text-start'>
             <Nav.Link>
-              <i className="fas fa-home"></i> Home
+              <i className='fas fa-fw fa-circle-h'></i>Home
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-regular fa-user"></i> Sign in
+              <i className='fas fa-fw fa-circle-user'></i>Sign in
             </Nav.Link>
             <Nav.Link>
-              <i className="fas fa-solid fa-user-plus"></i> New User
+              <i className='fas fa-fw fa-circle-plus'></i>New User
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
