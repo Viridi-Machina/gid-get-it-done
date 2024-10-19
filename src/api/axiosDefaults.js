6362d985a69126875ebe9e49f46b68f873cbea32
jacen green <jacen.l.green@hotmail.com>
2024-10-19 20:53:32 +0100

    add refresh token capability

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://gid-api-a5a5270599fa.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosRequest = axios.create();
+export const axiosResponse = axios.create();
